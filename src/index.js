ab34debdac8aba24c8e8b73e8b285204c7ff3c8f
Jegou, Pierre <pierre.jegou@cgi.com>
2022-09-24 14:05:33 +0200

    Feu artifice

--- src/index.js
+++ src/index.js
@@ -2,68 +2,77 @@
 const canvas = document.getElementById('my-canva');
 const ctx = canvas.getContext("2d");
 const button = document.getElementById('clearButtonId');
-const maxColorButton = document.getElementById('maxColorOn');
-const pencilSizeInput = document.getElementById('pencilSizeInput');
 
+const mousePosition = {
+    posX : null,
+    posY : null
+}
 
-let posX = null
-let posY = null
-let maxColor = false;
-let isMouseDown = false;
-let pencilSize = 50;
+const currentColor = {
+    red : 0,
+    green : 0,
+    bleu : 0
+}
 
 button.addEventListener('click', (event) => {
     ctx.clearRect(0, 0, canvas.offsetWidth, canvas.offsetHeight)
 });
 
-pencilSizeInput.addEventListener('change', (event) => {
-    pencilSize = event.target.value;
-})
+let particules = [];
 
-maxColorButton.addEventListener('click', (event) => {
-    if(maxColorButton.checked){
-        maxColor = true;
-    }else {
-        maxColor = false;
+class Particule{
+    constructor(posX, posY, ctx) {
+        this.posX = posX;
+        this.posY = posY;
+        this.ctx = ctx;
+        this.size = Math.random() * 5 + 1;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
     }
-})
 
-function drawCircle(posX, posY) {
-    ctx.beginPath();
-    ctx.arc(posX, posY, pencilSize, 0, 2 * Math.PI);
-    ctx.stroke();
-    ctx.fillStyle = getRndColor();
-    ctx.fill();
+    draw(){
+        ctx.beginPath();
+        ctx.arc(this.posX, this.posY, this.size, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.fillStyle = getRndColor();
+        ctx.fill();
 
-    function getRndColor() {
-        var r = 100*Math.random()|0,
-            g = 100*Math.random()|0,
-            b = 100*Math.random()|0;
-        if(maxColor){
+        function getRndColor() {
+            var r = 100*Math.random()|0,
+                g = 100*Math.random()|0,
+                b = 255*Math.random()|0;
             return 'rgb(' + r + ',' + g + ',' + b + ')';
-        }else{
-            return 'black'
         }
+
+        function getNextColor(){
+
+        }
+
     }
 
+    update(){
+        this.posX += this.speedX;
+        this.posY += this.speedY;
+    }
 }
 
-canvas.addEventListener('mousedown', (event) => {
-    isMouseDown = true;
+canvas.addEventListener('click', (event) => {
+    mousePosition.posX = event.x;
+    mousePosition.posY = event.y;
+    initParticules();
 });
 
-canvas.addEventListener('mouseup', (event) => {
-    isMouseDown = false;
-});
 
-canvas.addEventListener('mousemove', (event) => {
-    posX = event.x;
-    posY = event.y;
-});
+function initParticules(){
+    for(let i = 0; i < 100; i++){
+        particules.push(new Particule(mousePosition.posX, mousePosition.posY, ctx));
+    }
+}
 
 function animate(){
-    if(isMouseDown){
-        drawCircle(posX, posY);
+    for(let particule of particules){
+        particule.draw();
+        particule.update();
     }
     requestAnimationFrame(animate)
 }
