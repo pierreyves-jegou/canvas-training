75afb390e329e13d0be20c5325c9601af633d5e8
Jegou, Pierre <pierre.jegou@cgi.com>
2022-10-02 12:32:21 +0200

    scroll background

--- src/js/Game.js
+++ src/js/Game.js
@@ -1,14 +1,24 @@
 const canvas = document.getElementById('my-canva');
-canvas.width = window.innerWidth-10;
-canvas.height = window.innerHeight-10;
+canvas.width = window.innerWidth - 10;
+canvas.height = window.innerHeight - 10;
 const ctx = canvas.getContext("2d");
 
-class Game{
+class Game {
     constructor(width, height, ctx) {
         this.width = width;
         this.height = height;
         this.ctx = ctx;
+        this.scrollingInterval = {
+            left : 200,
+            right : 700
+        }
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
     }
+
+
 }
 
 export const game = new Game(canvas.width, canvas.height, ctx);
